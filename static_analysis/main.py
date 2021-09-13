0ec5163165d36c940983737703ed0dd767b73d0f
Justin C Presley <justincpresley@gmail.com>
2021-09-13 13:02:17 -0500

    Parse yaml file

--- static_analysis/main.py
+++ static_analysis/main.py
@@ -1,7 +1,20 @@
+import yaml
 
+def add_values(list, d):
+    for k,v in d.items():
+        if isinstance(v, dict):
+            add_values(list, v)
+        else:
+            print(k)
 
 def main():
-    pass
+    lists_of_values = []
+    with open('values.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        add_values(lists_of_values, data)
+
+    for i in lists_of_values:
+        print(i)
 
 if __name__=='__main__':
     main()
